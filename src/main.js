71e4687da0423e72a7780aa4963c860e08b5b8a1
Bere Frias <valkiriadehierro@yahoo.com>
2022-08-08 11:26:07 -0500

    header

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
+document.addEventListener("DOMContentLoaded", () => {
+  renderAthlete(femaleathletes)
+  total.textContent = femaleathletes.length;
+});
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
