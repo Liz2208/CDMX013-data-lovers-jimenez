ea286dab87c2c73f5b5b963e45b8b8952620fe15
Bere Frias <valkiriadehierro@yahoo.com>
2022-08-09 01:01:15 -0500

    navegacion,header diamico

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 
+window.addEventListener("scroll", function(){
+	var header = this.document.querySelector("header");
+	header.classList.toggle("abajo",window.scrollY>0);
+})
 
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-document.addEventListener("DOMContentLoaded", () => {
-  renderAthlete(femaleathletes)
-  total.textContent = femaleathletes.length;
-});
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
