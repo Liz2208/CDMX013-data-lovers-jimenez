73ffe6aa850da3779f64e1f47489ac894f019235
Liz2208 <j29lizeth@gmail.com>
2022-08-09 10:59:05 -0500

    prueba1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+
+window.addEventListener("scroll", function(){
+	var header = this.document.querySelector("header");
+	header.classList.toggle("abajo",window.scrollY>0);
+})
+
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
