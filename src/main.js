2b33d8ee22024ac3a42d0c1a68a9b33376b834d2
Bere Frias <valkiriadehierro@yahoo.com>
2022-08-10 13:31:21 -0500

    sobre este trabajar

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 1e4ad0beac611ced1948d0b25988451642447958
 window.addEventListener("scroll", function(){
 	var header = this.document.querySelector("header");
 	header.classList.toggle("abajo",window.scrollY>0);
 })
 
-<<<<<<< HEAD
 import  { example } from './data.js';
-=======
-import { example } from './data.js';
->>>>>>> 1e4ad0beac611ced1948d0b25988451642447958
-// import data from './data/lol/lol.js';
+
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 
 console.log(example, data);
