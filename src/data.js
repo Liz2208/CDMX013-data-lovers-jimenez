ff52021a4a7e2d861c42c072836da8004cff8341
Bere Frias <valkiriadehierro@yahoo.com>
2022-08-16 15:56:25 -0500

    data

--- src/data.js
+++ src/data.js
@@ -1,8 +1,31 @@
-// estas funciones son de ejemplo
+const functions = {
+    //Función ordenar de la A-Z
+  sortOrder: (element) => {
+    let out = element.sort((a,b) => a.name.localeCompare(b.name));
+    return out
+  },
 
-export const example = () => {
-  return 'example';
-};
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+  //Función ordenar de la Z-A
+  sortOrderReverse: (element) => {
+      let out = element.sort((a,b) => a.name.localeCompare(b.name));
+      out.reverse();
+      return out
+  },
+
+
+  //Función de filtrado por nombre
+  filterNames: (data, input) => {
+    return data.filter( (element) => {
+      if(element.name.toLowerCase() == input.toLowerCase()) {
+        return true
+      }else{
+        return false
+      }
+    })
+  }
+  
+}
+
+
+export default functions
--- src/main.js
+++ src/main.js
@@ -1,11 +1,37 @@
-window.addEventListener("scroll", function(){
-	var header = this.document.querySelector("header");
-	header.classList.toggle("abajo",window.scrollY>0);
+import data from './data/athletes/athletes.js';
+const section = document.getElementById('sectionAthletes');
+data.athletes.forEach(element => {
+	element["genderIcon"]=element.gender === 'F' ? element.gender='🙋🏻‍♀️': element.gender = '🙋🏻‍♂️'
+	if(element.medal==='Bronze'){
+		element["medalIcon"] = '🥉';
+		}
+		else if (element.medal ==='Silver'){
+			element["medalIcon"]  = '🥈';
+		}
+		else{
+			element["medalIcon"]  = '🥇';
+		}
+
+	let html= `
+	<section id="athletes">
+	<div class="name">  ${element.name} </div>
+	<div class="gender">Gender: ${element.genderIcon} </div>
+	<div class="sport">Sport: ${element.SportIcon} </div>
+	<div class="medal">Medal: ${element.medalIcon} </div>
+	<div class="gender">Team: ${element.team} </div>
+
+	</section>
+	<div class="card">
+	<div class="card-image"></div>
+	<div class="card-opacidad"></div>
+                <p><strong>${element.name}</strong><p>
+                <p>Gender: ${element.genderIcon}</p>
+				<p>Sport: ${element.sport}</p>
+                <p>Medal: ${element.medalIcon}</p>
+				<p>Team: ${element.team}</p>
+        </div>
+	`;
+	section.insertAdjacentHTML('beforebegin', html);
 })
 
-import  { example } from './data.js';
 
-import data from './data/pokemon/pokemon.js';
-
-
-console.log(example, data);
