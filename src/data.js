f6679f69dcc0bc313994836acca72c4048945f63
Liz2208 <j29lizeth@gmail.com>
2022-08-16 23:54:37 -0500

    actuaÃlizando images

--- src/data.js
+++ src/data.js
@@ -1,25 +1,50 @@
-export const sortData = (data, sortOrden) => {
-    if (sortOrden === 'ASC'){ 
-        data.sort ((a,b)=> {
-          if (a.name > b.name) {
-            return 1;
-          }
-          else if (a.name < b.name) {
-            return -1;
-          }
-            return 0;
-        })
+//Filtro por deporte
+export const filterSport =(athletes, str) => {
+  const resultBySport=athletes.filter(x=>x.sport===str)
+    return resultBySport;
+ }
+ //Filtro por pais
+ export const filterPais =(athletes, str2) => {
+  const resultByTeam=athletes.filter(x=>x.team===str2)
+    return resultByTeam; 
+ }
+ //Filtro por Medallas
+export const filterMedals =(athletes, str3) => {
+  const resultByMedals=athletes.filter(x=>x.medal===str3)
+    return resultByMedals;
+ }
+ //Filtro por Género
+ export const filterG =(athletes, str5) => {
+  const resultByGender=athletes.filter(x=>x.gender===str5)
+    return resultByGender;
+ }
+ //Ordenar ascendente o descentende
+ export const sortData = (data, sortOrder) => {
+  const ordenado=
+     data.sort ((a,b)=> {
+       if (a.name > b.name){
+         return 1;
+       }
+      else if (a.name<b.name){
+        return -1;
       }
-      else{
-        data.sort( (a,b) => {
-          if (a.name > b.name) {
-            return -1;
-          }
-          else if (a.name < b.name) {
-            return 1;
-          }
-            return 0;
-        })
-      }
-      return data;
-};
+      return 0;
+    });
+  if(sortOrder==="A-Z"){
+    return ordenado;
+  }
+  else if(sortOrder==="Z-A"){
+    return ordenado.reverse();
+  }
+}
+//Estadística porcentaje de participación por genero
+
+export const computeStats =(arrayFemale,arrayMale,arrayTotal)=>{
+const countFemale=arrayFemale.length;
+const countMale=arrayMale.length;
+const countTotal=arrayTotal.length;
+const percentFemale=Math.round((countFemale/countTotal)*100);
+const percentMale=Math.round((countMale/countTotal)*100);
+let array =[percentFemale, percentMale];
+return array;
+}
--- src/main.js
+++ src/main.js
@@ -1,58 +1,45 @@
-window.addEventListener("scroll", function() {
-	var header = this.document.querySelector("header");
-	header.classList.toggle("abajo",window.scrollY>0);
-})
+import data from './data/athletes/athletes.js';
+const section = document.getElementById('sectionAthletes');
 
-import data from "./data/athletes/athletes.js";
-//import{ sortData} from './data.js';
+data.athletes.forEach(element => {
+	element["genderIcon"]=element.gender === 'F' ? element.gender='🙋🏻‍♀️': element.gender = '🙋🏻‍♂️'
+	if(element.medal==='Bronze'){
+		element["medalIcon"] = '🥉';
+		}
+		else if (element.medal ==='Silver'){
+			element["medalIcon"]  = '🥈';
+		}
+		else{
+			element["medalIcon"]  = '🥇';
+		}
 
-export const dataForAthletes = data.athletes;
-//Llamamos al botón que nos mostrará la sección de deportes con sus dibujos:
-document.getElementById('sports-btn').addEventListener('click',()=>{
-    document.getElementById('intro').style.display='none';
-    document.getElementById('sectionSports').style.display='block'; 
-})
-
-///Verificamos qué deportes tiene la data:
-let arraySports = [];
-for(let element of dataForAthletes){
-    arraySports.push(element.sport);
-}
-
-//Eliminamos los deportes que se repiten:
-arraySports = arraySports.filter((item,index)=>{
-      return arraySports.indexOf(item) === index;
-})
-
-//Traemos el ID donde está la etiqueta "<table>" de HTML:
-let playersTable=document.getElementById('playersTable');
-
-//Creamos un bucle para que se muestre la data del deporte seleccionado de las tarjetas:
-let dataForSport = [];
-for(let i = 0; i<arraySports.length; i++){
-    let card= document.getElementsByClassName('card')[i];
-    card.addEventListener('click', ()=>{
-    document.getElementById('sectionSports').style.display='none';
-   // document.getElementById('medalTable').style.display='block';
-   // dataForSport  =  dataForSport.concat(filterData(dataForAthletes,card.getAttribute('value')));
-    playersTable.innerHTML = '';
-    createTable(dataForSport);
-    }
-)}
-
-//Creamos una función que se encargue de crear la tabla:
-const createTable = (array) => {
-    playersTable.innerHTML+=`<tr><th>NOMBRE</th>
-    <th>GÉNERO</th><th>PAÍS</th><th>EDAD</th>
-    <th>EVENTO</th><th>MEDALLA</th></tr>`;
-     
-    for(let i = 0; i<array.length; i++){
-        playersTable.innerHTML+=`<tr><td>${array[i].name}</td>
-        <td>${array[i].gender}</td>
-        <td>${array[i].team}</td>
-        <td>${array[i].age}</td>
-        <td>${array[i].event}</td>
-        <td>${array[i].medal}</td></tr>`;
-    }
-    return array;
-}
+		let html= `
+		<section id="athletes" class="card">
+		<div class="card-image"></div>
+		<div class="card-opacidad"></div>
+		<div class="name"> <p><strong> ${element.name}<strong> <p> </div>
+		<div class="gender"> <p>Gender: ${element.genderIcon} <p> </div>
+		<div class="sport"><p>Sport: ${element.sport} <p> </div>
+		<div class="medal"><p>Medal: ${element.medalIcon} <p> </div>
+		<div class="gender"><p>Team: ${element.team} <p> </div>
+		`;
+		
+		section.insertAdjacentHTML('beforebegin', html);
+		
+	})
+	let myheader = document.getElementById("header");
+	window.onscroll = function() {scrollFunction()};
+	
+	myheader.addEventListener("click",topFunction)
+	
+		function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		myheader.style.display = "flex";
+		} else {
+		myheader.style.display = "flex";
+		}
+		}
+		function topFunction() {
+			document.body.scrollTop = 0;
+			document.documentElement.scrollTop = 0;
+		}
