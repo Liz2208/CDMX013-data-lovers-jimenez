ddc8be46c406e6fec746a3f003e5779b4a77f23e
Liz2208 <j29lizeth@gmail.com>
2022-08-23 10:52:29 -0500

    filtros medio funcionado

--- src/data.js
+++ src/data.js
@@ -1,54 +1,21 @@
-<<<<<<< HEAD
-//Filtro por deporte
-export const filterSport =(athletes, str) => {
-  const resultBySport=athletes.filter(x=>x.sport===str)
-    return resultBySport;
- }
- //Filtro por pais
- export const filterPais =(athletes, str2) => {
-  const resultByTeam=athletes.filter(x=>x.team===str2)
-    return resultByTeam; 
- }
- //Filtro por Medallas
-export const filterMedals =(athletes, str3) => {
-  const resultByMedals=athletes.filter(x=>x.medal===str3)
-    return resultByMedals;
- }
- //Filtro por Género
- export const filterG =(athletes, str5) => {
-  const resultByGender=athletes.filter(x=>x.gender===str5)
-    return resultByGender;
- }
- //Ordenar ascendente o descentende
- export const sortData = (data, sortOrder) => {
-  const ordenado=
-     data.sort ((a,b)=> {
-       if (a.name > b.name){
-         return 1;
+export const filterGender=(data, genderIcon) => data.athletes.filter(athlete=>genderIcon===athlete.gender)
+export const sortData = (data, sortOrder) => {
+   const ordenado=
+      data.sort ((a,b)=> {
+        if (a.name > b.name){
+          return 1;
+        }
+       else if (a.name<b.name){
+         return -1;
        }
-      else if (a.name<b.name){
-        return -1;
-      }
-      return 0;
-    });
-  if(sortOrder==="A-Z"){
-    return ordenado;
-  }
-  else if(sortOrder==="Z-A"){
-    return ordenado.reverse();
-  }
+       return 0;
+     });
+   if(sortOrder==="A-Z"){
+     return ordenado;
+   }
+   else if(sortOrder==="Z-A"){
+     return ordenado.reverse();
+   }
 }
-//Estadística porcentaje de participación por genero
-
-export const computeStats =(arrayFemale,arrayMale,arrayTotal)=>{
-const countFemale=arrayFemale.length;
-const countMale=arrayMale.length;
-const countTotal=arrayTotal.length;
-const percentFemale=Math.round((countFemale/countTotal)*100);
-const percentMale=Math.round((countMale/countTotal)*100);
-let array =[percentFemale, percentMale];
-return array;
-}
-=======
-export const filterGender = (genero, data)=>  data.filter(character => character.gender == genero);
->>>>>>> a9ee011c8f802c81e22f13281d32a4daa8a9a61f
+export const filterSport=(data, sportID) => data.athletes.filter(athlete=>sportID===athlete.sport);
+export const filterteam=(data, teamID) => data.athletes.filter(athlete=>teamID===athlete.team);
--- src/main.js
+++ src/main.js
@@ -1,34 +1,95 @@
+import data from './data/athletes/athletes.js';
+import {filterGender, filterSport, filterteam} from './data.js'
+
 window.addEventListener("scroll", function() {
 	var header = this.document.querySelector("header");
 	header.classList.toggle("abajo",window.scrollY>0);
-	})
-	import data from './data/athletes/athletes.js';
-	const section = document.getElementById('sectionAthletes');
-	const button = document.getElementById("button1");
-	button.addEventListener("click",()=>{
-		section.classList.toggle("displaysection")
-	});
-	data.athletes.forEach(element => {
-		element["genderIcon"]=element.gender === 'F' ? element.gender='🙋🏻‍♀️': element.gender = '🙋🏻‍♂️'
-		if(element.medal==='Bronze'){
-			element["medalIcon"] = '🥉';
-			}
-			else if (element.medal ==='Silver'){
-				element["medalIcon"]  = '🥈';
-			}
-			else{
-				element["medalIcon"]  = '🥇';
-			}
-	
-		let html= `
+})
+
+const renderAthlete = (athlete) => {
+	return `
 		<section id="athletes" class="card">
 		<div class="card-image"></div>
 		<div class="card-opacidad"></div>
-		<div class="name"> <p><strong> ${element.name}<strong> <p> </div>
-		<div class="gender"> <p>Gender: ${element.genderIcon} <p> </div>
-		<div class="sport"><p>Sport: ${element.sport} <p> </div>
-		<div class="medal"><p>Medal: ${element.medalIcon} <p> </div>
-		<div class="gender"><p>Team: ${element.team} <p> </div>
-		`;
+		<div class="name"> <p><strong> ${athlete.name}<strong> <p> </div>
+		<div class="gender"> <p>Gender: ${athlete.genderIcon} <p> </div>
+		<div class="sport"><p>Sport: ${athlete.sport} <p> </div>
+		<div class="medal"><p>Medal: ${athlete.medalIcon} <p> </div>
+		<div class="gender"><p>Team: ${athlete.team} <p> </div>
+	`;
+	
+}
+ 
+const section = document.getElementById('sectionAthletes');
+const button = document.getElementById("button1");
+button.addEventListener("click",()=>{
+	section.classList.toggle("displaysection")
+});
+data.athletes.forEach(element => {
+	element["genderIcon"]=element.gender === 'F' ? element.gender='🙋🏻‍♀️': element.gender = '🙋🏻‍♂️'
+	if(element.medal==='Bronze'){
+		element["medalIcon"] = '🥉';
+	} else if (element.medal ==='Silver'){
+		element["medalIcon"]  = '🥈';
+	} else {
+		element["medalIcon"]  = '🥇';
+	}
+
+	const html = renderAthlete(element)
+	
+	section.insertAdjacentHTML('afterbegin', html);
+})
+	//filtro por genero
+const filterBtnGender = document.querySelector('.filterBtnGender')
+
+filterBtnGender.addEventListener('change', (event) => {
+	const genderId = event.target.value
+	const genderIcon =  genderId === 'F' ? '🙋🏻‍♀️': '🙋🏻‍♂️'
+	
+
+	filterGender(data, genderIcon).forEach(athlete => {
+		const html = renderAthlete(athlete)
 		section.insertAdjacentHTML('afterbegin', html);
+		console.log(filterGender)
 	})
+})
+
+// filtrado de deporte
+const filterBtnSport = document.querySelector('.filterBtnSport')
+
+filterBtnSport.addEventListener('change', (event) => {
+    const sportID = event.target.value
+	const sport =sportID === '' ? sport:
+
+	filterSport(data, sportID).forEach(sport => {
+		const html = renderAthlete(sport)
+		section.insertAdjacentHTML('afterbegin', html);
+		console.log(filterSport)
+			
+	})
+})
+// filtrado countries
+const filterBtnteam = document.querySelector('.filterBtnteam')
+
+filterBtnteam.addEventListener('change', (event) => {
+    const teamID = event.target.value
+	const team =teamID === '' ? team:
+
+	filterteam(data, teamID).forEach(team => {
+		const html = renderAthlete(team)
+		section.insertAdjacentHTML('afterbegin', html);
+		console.log(filterteam)
+			
+	})
+})
+
+//ordenacion por alfabeto
+/*const filterBtnSort = document.querySelector('.filterBtnSort')
+filterBtnSort.addEventListener('change', (event)=>{
+	const sortData=event.target.value
+	filterData(data, sortData).forEach(name => {
+		const html = renderAthlete(name)
+		section.insertAdjacentHTML('afterbegin', html);
+		console.log(filterBtnSort)
+	})
+})*/
