25435bef1c96e561ab39c39dc9c7253cc4a12bd7
Liz2208 <j29lizeth@gmail.com>
2022-08-24 09:47:56 -0500

    terminado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import data from './data/athletes/athletes.js';
-import {filterGender, filterSport, filterteam} from './data.js'
+import { filterGender, filterSport, filterteam, filterMedal } from "./data.js";
 
 window.addEventListener("scroll", function() {
 	var header = this.document.querySelector("header");
@@ -17,14 +17,23 @@ const renderAthlete = (athlete) => {
 		<div class="medal"><p>Medal: ${athlete.medalIcon} <p> </div>
 		<div class="gender"><p>Team: ${athlete.team} <p> </div>
 	`;
-	
+
 }
  
 const section = document.getElementById('sectionAthletes');
 const button = document.getElementById("button1");
 button.addEventListener("click",()=>{
+	ocultar();
 	section.classList.toggle("displaysection")
 });
+data.athletes.sort (function (a,b){
+	if(a.name.toUpperCase() > b.name.toUpperCase()){
+		return -1;
+	}
+	else {
+		return 1;
+	}
+})
 data.athletes.forEach(element => {
 	element["genderIcon"]=element.gender === 'F' ? element.gender='🙋🏻‍♀️': element.gender = '🙋🏻‍♂️'
 	if(element.medal==='Bronze'){
@@ -44,13 +53,12 @@ const filterBtnGender = document.querySelector('.filterBtnGender')
 
 filterBtnGender.addEventListener('change', (event) => {
 	const genderId = event.target.value
-	const genderIcon =  genderId === 'F' ? '🙋🏻‍♀️': '🙋🏻‍♂️'
+	const genderIcon =  genderId === 'F' ? '🙋🏻‍♀️' : '🙋🏻‍♂️'
 	
 
 	filterGender(data, genderIcon).forEach(athlete => {
 		const html = renderAthlete(athlete)
 		section.insertAdjacentHTML('afterbegin', html);
-		console.log(filterGender)
 	})
 })
 
@@ -58,38 +66,42 @@ filterBtnGender.addEventListener('change', (event) => {
 const filterBtnSport = document.querySelector('.filterBtnSport')
 
 filterBtnSport.addEventListener('change', (event) => {
+	section.innerHTML = ""
     const sportID = event.target.value
 	const sport =sportID === '' ? sport:
 
 	filterSport(data, sportID).forEach(sport => {
 		const html = renderAthlete(sport)
-		section.insertAdjacentHTML('afterbegin', html);
-		console.log(filterSport)
-			
+		section.insertAdjacentHTML('afterbegin', html);	
 	})
 })
 // filtrado countries
 const filterBtnteam = document.querySelector('.filterBtnteam')
 
 filterBtnteam.addEventListener('change', (event) => {
+	section.innerHTML = ""
     const teamID = event.target.value
 	const team =teamID === '' ? team:
 
 	filterteam(data, teamID).forEach(team => {
 		const html = renderAthlete(team)
 		section.insertAdjacentHTML('afterbegin', html);
-		console.log(filterteam)
-			
 	})
 })
 
-//ordenacion por alfabeto
-/*const filterBtnSort = document.querySelector('.filterBtnSort')
-filterBtnSort.addEventListener('change', (event)=>{
-	const sortData=event.target.value
-	filterData(data, sortData).forEach(name => {
-		const html = renderAthlete(name)
-		section.insertAdjacentHTML('afterbegin', html);
-		console.log(filterBtnSort)
-	})
-})*/
+// filtrado medallas
+const filterBtnMedal = document.querySelector(".filterBtnMedal");
+
+filterBtnMedal.addEventListener("change", (event) => {
+  section.innerHTML = "";
+  const medalId = event.target.value;
+
+  const medal= medalId === "" ? medal : filterMedal(data, medalId).forEach((medal) => {
+    const html = renderAthlete(medal);
+    section.insertAdjacentHTML("afterbegin", html);
+    console.log(filterMedal);
+  });
+});
+function ocultar(){
+	document.getElementById('videointro').style.display = 'none';
+	}
